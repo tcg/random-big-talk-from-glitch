c51066743a327fad79b596b3e3de7aa6834abb66
Glitch (glitch-blank-node) <none>
2021-10-02 04:17:06 +0000

    🚖🌹 Checkpoint ./server.js:9610734/848 ./src/pages/index.hbs:9610734/184

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const fs = require('fs');
+
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
@@ -26,20 +28,21 @@ fastify.register(require("point-of-view"), {
 fastify.get("/", function(request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = {
-    greeting: "Hello Node!"
+    greeting: "Hello Node!", 
+    quotes: quotesFileContent()
   };
   // request.query.paramName <-- a querystring example
   reply.view("/src/pages/index.hbs", params);
 });
 
 // A POST route to handle form submissions
-fastify.post("/", function(request, reply) {
-  let params = {
-    greeting: "Hello Form!"
-  };
-  // request.body.paramName <-- a form post example
-  reply.view("/src/pages/index.hbs", params);
-});
+// fastify.post("/", function(request, reply) {
+//   let params = {
+//     greeting: "Hello Form!"
+//   };
+//   // request.body.paramName <-- a form post example
+//   reply.view("/src/pages/index.hbs", params);
+// });
 
 // Run the server and report out to the logs
 fastify.listen(process.env.PORT, function(err, address) {
@@ -50,3 +53,21 @@ fastify.listen(process.env.PORT, function(err, address) {
   console.log(`Your app is listening on ${address}`);
   fastify.log.info(`server listening on ${address}`);
 });
+
+
+
+/**
+ * Return a list (array) of quotes.
+ * 
+ * The file is sectioned by "source". We aren't concerned about that 
+ * for now. Except that we don't want to return sources. 
+ * All "sections" look like this: 
+ *   [source: ... ]
+ * Therefore, we just don't return any lines that start with a bracket. 
+ * Because no legitimate quote/question will start with one anyway. 👍🏻
+ */
+const quotesFileContent = function() {
+  
+  const quotes = fs.readFileSync('src/quotes.txt', 'utf8').toString().split('\n').map( (item) => { if (item) });
+  return quotes
+}
